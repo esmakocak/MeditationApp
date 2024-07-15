0f773f4446e300643d669ac40f40317ba313e5cd
Esma Koçak <esma.kocaakk20@gmail.com>
2024-07-15 13:22:20 +0300

    Implement Extensions for formatting time

--- Meditation/Extensions.swift
+++ Meditation/Extensions.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+extension DateComponentsFormatter {
+    static let abbreviated: DateComponentsFormatter = {
+        print("Initializing DateComponentsFormatter.abbreviated")
+        let formatter = DateComponentsFormatter()
+        
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        
+        return formatter
+    }()
+    
+    static let positional: DateComponentsFormatter = {
+        print("Initializing DateComponentsFormatter.positional")
+        let formatter = DateComponentsFormatter()
+        
+        formatter.allowedUnits = [.minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        
+        return formatter
+    }()
+
+}
